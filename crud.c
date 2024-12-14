adba118443fea8304ec055c0e76ec649162f97e9
Vishwas Vijay Vargiya <vijayvargiyavishwas@gmail.com>
2024-12-14 19:44:49 +0530

    Mentor review changes

--- crud.c
+++ crud.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<stdbool.h>
-struct User{//structure for user data
+struct User{
     int id,age;
     char name[50];
 };
 
-void createFile(){//function to create new file
+void writeFile(struct User user,FILE* file){
+    fwrite(&user,sizeof(struct User),1,file);
+}
+bool readUser(FILE* file, struct User* user) {
+    return fread(user, sizeof(struct User), 1, file) == 1;
+}
+
+void createFile(){
     FILE* file = fopen("users.txt", "a+");
     if(file == NULL){
         printf("Error opening file\n");
@@ -14,10 +22,14 @@ void createFile(){//function to create new file
     fclose(file);
 }
 bool userPresent(int id){
-    FILE* file = fopen("users.txt", "r");//opening users.txt file in reading mode
+    FILE* file = fopen("users.txt", "r");
+    if(file==NULL){
+        printf("Error opening file\n");
+        exit(1);
+    }
     struct User user;
-    while(fread(&user,sizeof(struct User),1,file)){//reading each record from file
-        if(user.id == id){//if user id matches then returning 1 else 0
+    while(readUser(file,&user)){
+        if(user.id == id){
             fclose(file);
             return 1;
         }
@@ -26,7 +38,11 @@ bool userPresent(int id){
     return 0;
 }
 void createUser(){
-    FILE* file = fopen("users.txt", "a");//opening users.txt file in append mode
+    FILE* file = fopen("users.txt", "a");
+    if(file==NULL){
+        printf("Error opening file\n");
+        exit(1);
+    }
     struct User user;
     printf("Enter ID: ");
     scanf("%d",&user.id);
@@ -34,8 +50,8 @@ void createUser(){
     scanf("%s", user.name);
     printf("Enter Age: ");
     scanf("%d",&user.age);
-    if(!userPresent(user.id)){//checking if user already exists
-        fwrite(&user,sizeof(struct User),1,file);
+    if(!userPresent(user.id)){
+        writeFile(user,file);
         printf("User created successfully\n");
     }
     else   
@@ -43,64 +59,78 @@ void createUser(){
     fclose(file);
 }
 void readData(){
-    FILE* file = fopen("users.txt", "r");//opening users.txt file in read mode
+    FILE* file = fopen("users.txt", "r");
+    if(file==NULL){
+        printf("Error opening file\n");
+        exit(1);
+    }
     struct User user;
     printf("ID\t\tName\t\tAge\n");
-    while(fread(&user,sizeof(struct User),1,file)){//reading each record from file
+    while(readUser(file,&user)){
         printf("%d\t\t%s\t\t%d\n",user.id,user.name,user.age);
     }
     fclose(file);
 }
 
 void updateUser(){
-    FILE* file = fopen("users.txt", "r");//opening users.txt file in read mode
-    FILE* tempFile=fopen("tempUsers.txt","a");//opening temp file in append mode
-    int id;//id of user to update
+    FILE* file = fopen("users.txt", "r");
+    FILE* tempFile=fopen("tempUsers.txt","a");
+    if(file==NULL || tempFile==NULL){
+        printf("Error opening file\n");
+        exit(1);
+    }
+    int id;
     struct User user;
     printf("Enter ID of user to update: ");
     scanf("%d",&id);
-    while(fread(&user,sizeof(struct User),1,file)){//reading each record from file
+    while(readUser(file,&user)){
         if(user.id != id){
-            fwrite(&user,sizeof(struct User),1,tempFile);
+            writeFile(user,tempFile);
         }
-        else{//if user id matches then we need to update the user details
+        else{
             printf("Enter New Name: ");
             scanf("%s", user.name);
             printf("Enter New Age: ");
             scanf("%d",&user.age);
-            fwrite(&user,sizeof(struct User),1,tempFile);
+            writeFile(user,tempFile);
+            printf("User updated\n");
         }
     }
-    printf("User updated\n");
     fclose(tempFile);
     fclose(file);
 
-    remove("users.txt");//removing the user.txt file
-    rename("tempUsers.txt","users.txt");//renaming temp file as users.txt
+    remove("users.txt");
+    rename("tempUsers.txt","users.txt");
 
 }
 
 void deleteUser(){
-    FILE* file = fopen("users.txt", "r");//opening users.txt file in read mode
-    FILE* tempFile=fopen("tempUsers.txt","a");//opening temp file in append mode
+    FILE* file = fopen("users.txt", "r");
+    FILE* tempFile=fopen("tempUsers.txt","a");
+    if(file==NULL || tempFile==NULL){
+        printf("Error opening file\n");
+        exit(1);
+    }
     struct User user;
-    int id;//id of user to delete
+    int id;
     printf("Enter ID of user to delete: ");
     scanf("%d",&id);
-    while(fread(&user,sizeof(struct User),1,file)){//reading each record from file
-        if(user.id != id){//if user id not exists then writing in another file
-            fwrite(&user,sizeof(struct User),1,tempFile);
+    while(readUser(file,&user)){
+        if(user.id != id){
+            writeFile(user,tempFile);
+        }
+        else{
+            printf("User deleted\n");
         }
     }
-    printf("User deleted\n");
     fclose(tempFile);
     fclose(file);
 
-    remove("users.txt");//removing the user.txt file
-    rename("tempUsers.txt","users.txt");//renaming temp file as users.txt
+    remove("users.txt");
+    rename("tempUsers.txt","users.txt");
 }
 void main(){
-    createFile();//creating a file if not exists
+    createFile();
     int choice=0;
     while(choice!=-1){
         printf("\nWhich operation do you want to perform\n1. Create User\n2. Read\n3. Update User\n4. Delete User\n5. Exit\nYour choice: ");
