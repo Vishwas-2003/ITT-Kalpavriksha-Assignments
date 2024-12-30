1d2f95aaf6654f2ceb92a5a8c7172e6609e41237
Vishwas Vijay Vargiya <vijayvargiyavishwas@gmail.com>
2024-12-30 18:15:50 +0530

    power mod question implementation

---
+++ powerMod.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int calculateMod(int a,int b,int c){
+    int result=1;
+    a=a%c;
+    while(b>0){
+        if(b%2==1) 
+            result=(result*a)%c;
+        a=(a*a)%c;
+        b=b/2;
+    }
+    return result;
+}
+void main(){
+    int a,b,c;
+    printf("Enter the value of a,b,c:");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("%d",calculateMod(a,b,c));
+}
